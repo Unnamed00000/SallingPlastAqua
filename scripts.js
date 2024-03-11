d3a6179e01e182cd5d5745fe716ab6c6f8bbf598
Unnamed00000 <157989745+Unnamed00000@users.noreply.github.com>
2024-03-12 00:47:57 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var currentContent = null;
+
+function showContent(contentId) {
+  // Скрывает текущий блок контента, если он существует
+  if (currentContent !== null) {
+    currentContent.classList.add('hidden');
+  }
+
+  // Показывает выбранный блок контента
+  var selectedContent = document.getElementById(contentId);
+  selectedContent.classList.remove('hidden');
+
+  // Обновляет текущий блок контента
+  currentContent = selectedContent;
+}
+
